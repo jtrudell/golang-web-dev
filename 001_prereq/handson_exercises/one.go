3991d399dbdb78606eb33b8bf88cd02e7c8d497f
Jen <jtrudell@gmail.com>
2017-08-24 15:04:30 -0500

    solution to hands-on exercises one

---
+++ 001_prereq/handson_exercises/one.go
@@ -0,0 +1,47 @@
+// - create a type square
+// - create a type circle
+// - attach a method to each that calculates area and returns it
+// - create a type shape which defines an interface as anything which has the area method
+// - create a func info which takes type shape and then prints the area
+// - create a value of type square
+// - create a value of type circle
+// - use func info to print the area of square
+// - use func info to print the area of circle
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	side float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+func (s square) area() float64 {
+	return s.side * s.side
+}
+
+func (c circle) area() float64 {
+	return math.Pi * (c.radius * c.radius)
+}
+
+func info(s shape) {
+	fmt.Println("The area of your shape is", s.area())
+}
+
+func main() {
+	s := square{4.0}
+	c := circle{2.0}
+	info(s)
+	info(c)
+}
